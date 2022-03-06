61037e6faa7c9629d7535869ccdad8271584b834
rektcherry <100161898+rektcherry@users.noreply.github.com>
2022-03-06 18:31:52 +0200

    removed issues

--- config.js
+++ config.js
@@ -1,22 +1,6 @@
 var  fs  = require("fs");
-var txt = fs.readFileSync("./textfile.txt", "utf-8");
-var names = txt.split("\n"); 
-var step = 3 ; //step to go through txt file
-
-// these store the words and are later used to make combinations
-const name1 = []; 
-const name2 = [];
-const name3 = [];
-
-// this divides the text file into three name lists
-for (i = 0; i < names.length; i=i+step) {
-    name1.push(names[i]);
-}
-for (i = 1; i < names.length; i=i+step) {
-    name2.push(names[i]);
-  }
-for (i = 2; i < names.length; i=i+step) {
-    name3.push(names[i]);
-  }
-
-module.exports = {name1, name2, name3};
+var adj = fs.readFileSync("./nounFile.txt", "utf-8")
+adj = adj.toString().replace(/\r\n/g,'\n').split('\n');
+var noun = fs.readFileSync("./adjFile.txt", "utf-8");
+noun = noun.toString().replace(/\r\n/g,'\n').split('\n');
+module.exports = {adj, noun};
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const {name1, name2, name3} = require("./config.js");
-const format  = [name1,name2,name3];
+var {adj, noun} = require("./config.js");
+const format  = [adj,noun];
 const output_nmbr = 10; // add the number of names to generate
-const delimiter = ""; // add delimeter between words
 
-for (let i=0 ;i <output_nmbr; i++){ 
-    const create = () => {
-        let output =[];
+const create = () => {  // start creating
+    let output = []
+    for (let i=0 ;i < output_nmbr; i++){ 
         format.forEach((list) => {
-            let word = list[Math.floor(Math.random() * list.length)];
-            output.push(word);
-        })
-        return output.join(delimiter).replace(/\,/g, '');
-    }
-    console.log(create());
-}
+            let word = list[Math.floor(Math.random() * list.length)]; // get random word
+            output.push(word); // push word to output array
+        }) 
+        output.push('\n'); //add line break to separate words
+    }   
+    return output.join('').slice(0,-1); // join words on the same line and remove last linebreak
+}  
+console.log(create());
